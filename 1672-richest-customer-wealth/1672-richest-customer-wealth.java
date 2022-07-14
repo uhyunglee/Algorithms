d4b427e5be9fb2d7171599fab77762989dd33b27
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2022-07-14 17:26:53 +0900

    Time: 1 ms (52.26%), Space: 44.3 MB (5.90%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,18 +1,13 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int sum=0;
         int wealth =0;
        for(int i=0;i<accounts.length;i++){
+           int sum = 0;
            for(int j=0;j<accounts[i].length;j++){
               sum+=accounts[i][j]; 
            }
-              if(wealth < sum){
-                  wealth = sum;
-              }else if(wealth == sum){
-                  wealth = sum;
-              }
-               sum = 0;
+           wealth = Math.max(sum, wealth);
        }
-        return wealth;
+       return wealth;
     }
 }
