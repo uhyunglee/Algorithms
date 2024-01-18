fd0b631e2c422a71495f173d18846fc1ffb99766
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-01-18 14:08:52 +0900

    Time: 4 ms (65.54%), Space: 44.4 MB (48.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-        
-        for(int i = 0; i < nums.length - 1; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    answer[0] = i;
-                    answer[1] = j;
-                }
-            }
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        //키와 값을 바꿔서 맵으로 저장
+        for(int i = 0; i < nums.length; i++){
+            numsMap.put(nums[i], i);
         }
         
-        return answer;
+        //target에서 첫번째 수를 뺀 결과를 키로 조회하고 현재 인덱스가 아닌 경우 정답으로 리턴 
+        for(int i = 0; i <nums.length; i++){
+            if(numsMap.containsKey(target-nums[i]) && i != numsMap.get(target-nums[i]))
+                return new int[] {i, numsMap.get(target-nums[i])};
+        }
+        //항상 정답이 존재하므로 널이 리턴되는 경우는 없음
+        return null;
     }
 }
