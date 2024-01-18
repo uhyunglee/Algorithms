780b94eaa259e5192b64f7ef41cc478a3688235f
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-01-18 15:09:56 +0900

    Time: 3 ms (69.66%), Space: 45.2 MB (7.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,7 +5,7 @@ class Solution {
         //target에서 첫번째 수를 뺀 결과를 키로 조회하고 현재 인덱스가 아닌 경우 정답으로 리턴 
         for(int i = 0; i <nums.length; i++){
             if(numsMap.containsKey(target-nums[i]))
-                return new int[] {numsMap.get(target-nums[i]), i};
+                return new int[] {i, numsMap.get(target-nums[i])};
             //정답이 아니면 비교를 ㅟ해 인덱스를 맵에 저장
             numsMap.put(nums[i], i);
         }
