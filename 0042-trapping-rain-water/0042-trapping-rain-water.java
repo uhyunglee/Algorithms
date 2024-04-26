5dd67d732e6d3d04fb47d85cee7f9826a0b67837
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-04-26 17:54:08 +0900

    Time: 1 ms (71.27%), Space: 46.2 MB (36.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,28 @@
+import java.lang.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int trap(int[] height) {
-        Deque<Integer> stack = new ArrayDeque<>();
         int volume = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
         
-        for(int i = 0; i < height.length; i++){
-            // 변곡점을 만나는 경우
-            while(!stack.isEmpty() && height[i] > height[stack.peek()]){
-                // 스택에서 꺼낸다
-                Integer top = stack.pop();
-                
-                if(stack.isEmpty()) break;
-                
-                // 스택의 마지막 위치까지를 거리로 계산 
-                int distance = i - stack.peek() - 1;
-                // 현재 높이 또는 스택의 마지막 위치 높이 중 낮은 값에 방금 꺼낸 높이의 차이를 물 높이로 결정
-                int waters = Math.min(height[i], height[stack.peek()]) - height[top];
-                
-                // 물이 쌓이는 양은 거리와 물 높이의 곱
-                volume += distance * waters;
-            }
+        while(left < right){
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
             
-            stack.push(i);
+            // 더 높은 쪽으로 투포인터 이동
+            if (leftMax <= rightMax) {
+                // 왼쪽 막대 최대 높이와의 차이를 더하고 왼쪽 포인터 이동
+                volume += leftMax - height[left];
+                left += 1;
+            }else {
+                volume += rightMax - height[right];
+                right -= 1;
+            }
         }
         return volume;
     }
