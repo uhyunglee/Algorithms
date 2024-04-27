53a497aef96613aaa3462142afe415dc040ae35a
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-04-27 16:14:08 +0900

    Time: 0 ms (100.00%), Space: 46.1 MB (44.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,21 @@
-import java.lang.*;
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int trap(int[] height) {
-        int volume = 0;
         int left = 0;
         int right = height.length - 1;
         int leftMax = height[left];
         int rightMax = height[right];
+        int volume = 0;
         
-        while(left < right){
+        while(left<right){
             leftMax = Math.max(height[left], leftMax);
             rightMax = Math.max(height[right], rightMax);
             
-            // 더 높은 쪽으로 투포인터 이동
-            if (leftMax <= rightMax) {
-                // 왼쪽 막대 최대 높이와의 차이를 더하고 왼쪽 포인터 이동
+            if(leftMax <= rightMax){
                 volume += leftMax - height[left];
-                left += 1;
-            }else {
+                left++;
+            }else{
                 volume += rightMax - height[right];
-                right -= 1;
+                right--;
             }
         }
         return volume;
