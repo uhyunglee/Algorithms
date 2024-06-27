9715271719b306f83b3fbc2b55bf42e35704ee66
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-06-27 14:23:56 +0900

    Time: 1 ms (99.79%), Space: 61.6 MB (39.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int high = 0, low = prices[0];
+        int lowPrice = prices[0];
+        int profit = 0;
         
         for(int price : prices){
-            low = Math.min(low, price);
-            high = Math.max(high, price - low);
+            lowPrice = Math.min(lowPrice, price);
+            profit = Math.max(profit, price - lowPrice);
         }
-        return high;
+        
+        return profit;
     }
 }
