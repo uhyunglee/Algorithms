f38c5447b8375932c768fa7003e5216d1de718c7
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-07-05 18:36:38 +0900

    Time: 1 ms (100.00%), Space: 44.5 MB (22.20%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,51 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.BigInteger;
 class Solution {
-    public ListNode reverseList(ListNode head){
-        ListNode prev = null, node = head;
-        
-        while(node != null){
-            // 현재 노드의 다음 노드 미리 지정
-            ListNode next = node.next;
-            // 현재 노드의 다음으로 이전 노드 지정
-            node.next = prev;
-            // 이전 노드는 현재 노드로 지정
-            prev = node;
-            // 미리 지정했던 다음노드를 현재로 변경
-            node = next;
-        }
-        return prev;
-    }
-    
-    public BigInteger toBigInt(ListNode node){
-        String val = "";
-        
-        while(node != null){
-            val += node.val;
-            node = node.next;
-        }
-        
-        return new BigInteger(val);
-    }
-    
-    public ListNode toReversedLinkedList(BigInteger val){
-        ListNode prev = null, node = null;
-        // 정수형 -> 문자, 한글자씩 순회
-        for(char c : String.valueOf(val).toCharArray()){
-            node = new ListNode(Character.getNumericValue(c));
-            node.next = prev;
-            prev = node;
-        }
-        return node;
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l1Reversed = reverseList(l1);
-        ListNode l2Reversed = reverseList(l2);
+        ListNode node = new ListNode(0);
+        ListNode root = node;
         
-        BigInteger result = toBigInt(l1Reversed).add(toBigInt(l2Reversed));
+        int sum, carry = 0, remainder;
         
-        return toReversedLinkedList(result);
+        while(l1 != null || l2 != null || carry != 0){
+            sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            remainder = (sum + carry) % 10;
+            carry = (sum + carry) / 10;
+            node.next = new ListNode(remainder);
+            node= node.next;
+        }
+        return root.next;
     }
 }
